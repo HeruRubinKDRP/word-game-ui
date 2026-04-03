3104f7c200ba761eb517d3170c7881a565ce77c1
HeruRubinKDRP <herurubin@gmail.com>
2026-04-02 21:18:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // ===============================
 
 // Change this to your real deployed Vercel API URL when needed
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://word-game-api.vercel.app/";
 
 // ===============================
 // DOM references
